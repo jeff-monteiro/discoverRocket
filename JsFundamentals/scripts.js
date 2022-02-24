c6e378d4b1c448d160cdf1d5b8f02584a51c54f9
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-02-23 21:48:52 -0300

    Arrays

--- JsFundamentals/scripts.js
+++ JsFundamentals/scripts.js
@@ -1,5 +1,5 @@
 // Object
-
+/*
 const person = {
   name: 'Jeff',
   age: 27,
@@ -7,3 +7,18 @@ const person = {
   isAdmin: true
 }
 console.log(person)
+*/
+
+// Arrays
+
+const animals = [
+  'Lion',
+  'Cat',
+  {
+    name: 'Dog',
+    isAnimal: true
+  },
+  'Monkey'
+]
+// Acessando valores de dentro do Array
+console.log(animals[2].isAnimal)
