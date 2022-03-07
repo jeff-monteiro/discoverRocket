930828bb564d97196d24148d63139235e4a087c8
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-07 16:28:40 -0300

    functionsJS

---
+++ JsFundamentals/functions/functions.js
@@ -0,0 +1,9 @@
+// criar um aplicativo de frases motivacionais
+
+// declaration - declaração da função
+// funcrion statement
+function createPhrases(){
+  console.log('Esperei com paciência no Senhor')
+  console.log('Paciência e persistência')
+  console.log('Revisão é mãe do aprendizado')
+}
