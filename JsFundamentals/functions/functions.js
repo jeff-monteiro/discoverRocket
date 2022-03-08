f5f6cbd1e388171a71465dffc3402fe6a7ba24fd
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-08 16:24:36 -0300

    functionScope

--- JsFundamentals/functions/functions.js
+++ JsFundamentals/functions/functions.js
@@ -1,7 +1,12 @@
 // function scope
 
-let subject
+let subject = 'create video'
 
-function 
+function createThink(subject){
+  subject = 'study'
+  return subject
+}
 
+console.log(createThink(subject))
+console.log(subject)
 
