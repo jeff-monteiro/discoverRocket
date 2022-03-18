2b4481dd858d2802eeb9ec4128a5e6f8fa33fac6
Jefferson Monteiro <81328013+jeff-monteiro@users.noreply.github.com>
2022-03-18 09:13:58 -0300

    manipulatingData

--- JsFundamentals/functions/prototype.js
+++ JsFundamentals/functions/prototype.js
@@ -17,6 +17,10 @@
     * Alteração de um tipo de dado para outro tipo
 */
 
+// Manipulando Strings e Números
+
+// Transformar String em Número e Número em String
+
 let string = "123"
 console.log(Number(string))
 let number = 321
