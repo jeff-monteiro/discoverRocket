5f6fbf60b929286c059b66a2045d374c383cb264
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-26 17:34:33 -0300

    expressions&operators

---
+++ JsFundamentals/expression&operators/expressions.js
@@ -0,0 +1,39 @@
+/* Expressions & Operators
+
+- Expressions
+- Operators
+    Binary
+    Unary
+    Ternary
+*/
+
+/*
+
+let number = 1
+
+// Operador binário
+console.log(number + 1)
+// Operador unário
+console.log(++number)
+// Operador ternário
+console.log(false ? 'alo' : 'nothing')
+
+*/
+
+/*
+
+    new
+
+    *left-hand-side expression
+    *Criar um novo objeto
+
+*/
+
+let date = new Date('2022-03-26')
+console.log(date)
+
+let name = new String('Jeff')
+name.surName = 'Monteiro'
+let age = new Number(26)
+console.log(name, age)
+console.log(name.surName)
