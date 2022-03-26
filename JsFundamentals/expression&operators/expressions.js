5a7382951f47f66193b0c54526569937ae1d4d6c
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-26 18:16:51 -0300

    arithimeticsOperators

--- JsFundamentals/expression&operators/expressions.js
+++ JsFundamentals/expression&operators/expressions.js
@@ -37,3 +37,28 @@ name.surName = 'Monteiro'
 let age = new Number(26)
 console.log(name, age)
 console.log(name.surName)
+
+/*
+    Operadores unários
+    typeof
+    delete
+
+*/
+
+console.log(typeof age)
+
+const person = {
+    name: "Moura",
+    age: 26,
+}
+delete person.name
+console.log(person)
+
+let totalTwo = 2 + 3 * 5
+console.log(totalTwo)
+//com essa precedência, nosso resultado é 17.
+
+let total = (2 + 3) * 5
+console.log(total)
+// o cálculo mudou a precedência, fazendo nosso resultado tornar-se 25.
+
