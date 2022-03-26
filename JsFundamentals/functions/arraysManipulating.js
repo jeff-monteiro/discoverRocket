d25ea6a026865c0d3955d2aa7ea7852378ce1782
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-25 23:25:05 -0300

    arraysManipulating

---
+++ JsFundamentals/functions/arraysManipulating.js
@@ -0,0 +1,29 @@
+// Manipulando Arrays
+
+let techs = [ 'html', 'css', 'js']
+
+// adicionar um item no fim
+techs.push('nodejs')
+
+// adicionar no começo
+techs.unshift('react')
+
+// remover do fim
+// techs.pop()
+
+// remover do começo
+// techs.shift()
+
+// pegar somente alguns elementos do array
+// console.log(techs.slice(1, 3))
+
+// remover 1 ou mais itens em qualquer posição do array
+// techs.splice(1, 2)
+
+// encontrar a posição de um elemento no array
+let index = techs.indexOf('js')
+// techs.splice(index, 1)
+
+console.log(techs)
+console.log(index)
+
