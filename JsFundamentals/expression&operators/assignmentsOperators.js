98775e9d92960500ae88ef47f6b499543b87de00
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-27 22:57:39 -0300

    assignmentOperators

---
+++ JsFundamentals/expression&operators/assignmentsOperators.js
@@ -0,0 +1,18 @@
+let x
+
+
+// assignment
+x = 1
+
+// addition assignment
+x += 2
+console.log(x)
+
+// subtraction assignment
+x -= 1
+
+// multiplication assignment
+x *= 2
+
+// division assignment
+x /= 2
