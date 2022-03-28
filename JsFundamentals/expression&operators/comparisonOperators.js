041661ac929a4205b576031dd833cc7672e94c6f
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-27 21:10:34 -0300

    comparisonOperators

--- JsFundamentals/expression&operators/comparisonOperators.js
+++ JsFundamentals/expression&operators/comparisonOperators.js
@@ -9,7 +9,7 @@ let two = 2
 console.log(two == 1)
 console.log(one == '1')
 
-// =! diferente de
+// != diferente de
 console.log(one != two)
 console.log(one != 1)
 console.log(one != '1')
@@ -21,3 +21,8 @@ console.log(one === 1)
 // !== estritamente diferente de
 console.log(two !== '2')
 console.log(two !== 2)
+
+// > Maior que
+// < Menor que
+// >= Maior igual a
+// <= Menor igual a
