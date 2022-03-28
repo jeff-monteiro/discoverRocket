505d3daebbbc22a08409d1292108e07eb704484a
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-27 23:09:19 -0300

    logicalOperators

---
+++ JsFundamentals/expression&operators/logicalOperators.js
@@ -0,0 +1,12 @@
+// Operadores lógicos
+
+// 2 valores booleanos, quando verificados, resultará em verdadeiro ou falso
+
+let pao = true
+let queijo = false
+
+// AND &&
+console.log(pao && queijo)
+// OR ||
+
+// NOT !
