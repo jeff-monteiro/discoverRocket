cdf25ac6ba22f12ead1a593478fd723808137735
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-27 21:04:43 -0300

    comparisonOperators

---
+++ JsFundamentals/expression&operators/comparisonOperators.js
@@ -0,0 +1,23 @@
+// Operadores de Comparação
+
+// Iŕa comparar valores e retornar um Boolean como resposta à comparação
+
+let one = 1
+let two = 2
+
+// == igual a
+console.log(two == 1)
+console.log(one == '1')
+
+// =! diferente de
+console.log(one != two)
+console.log(one != 1)
+console.log(one != '1')
+
+// === estritamente igual a
+console.log(one === '1')
+console.log(one === 1)
+
+// !== estritamente diferente de
+console.log(two !== '2')
+console.log(two !== 2)
