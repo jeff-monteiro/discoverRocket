fda8bffa5172bee285064b688505f61d26065218
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-28 23:27:35 -0300

    ternaryOperator

---
+++ JsFundamentals/expression&operators/ternaryOperators.js
@@ -0,0 +1,9 @@
+// Operador Condicional Ternário
+let bread = true
+let cheese = false
+const theGoodBreakFast = bread || cheese ? "So good" : "Not so good"
+console.log(theGoodBreakFast)
+
+let age = 18
+const verificationAge = age >= 18 ? "Maior de idade" : "Menor de idade"
+console.log(verificationAge)
