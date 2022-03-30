677918112e9befc7694980a2c9cc8f52e55f9df6
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-29 23:51:06 -0300

    precedenceOperator

---
+++ JsFundamentals/expression&operators/operatorPrecedence.js
@@ -0,0 +1,17 @@
+// De cima para baixo, do mais importante ao menos importante.
+// Operador de Precedência
+/*
+
+* grouping                      ( )
+* negação e incremento          ! ++ --
+* multiplicação e divisão       * /
+* adição e subtração            + -
+* relacional                    < <= > >=
+* igualdade                     == != === !==
+* AND                           && 
+* OR                            ||
+* condicional                   ?:
+* assignment (atribuição)       = += -= *= %=
+
+*/
+console.log((2 + 5) * 9)
