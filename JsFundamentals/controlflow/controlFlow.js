2b4668e9517413331a86ed985f3076dd0010e640
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-04-01 16:51:58 -0300

    controlFlow

--- JsFundamentals/controlflow/controlFlow.js
+++ JsFundamentals/controlflow/controlFlow.js
@@ -1,5 +1,14 @@
 // Control Flow
 
-console.log('a')
-console.log('b')
-console.log('jeff')
+// if...else
+
+let temperature = 36.9
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37 && temperature <= 36.9
+
+if(highTemperature){
+  console.log(highTemperature)
+ } else if (mediumTemperature){
+   console.log()
+ }
+
